0113811dab1d26888006615858ed8877d0533b29
Appledog25 <adititayde2509@gmail.com>
2021-10-04 16:57:36 +0530

    update

--- Task5/js/android.js
+++ Task5/js/android.js
@@ -6,47 +6,47 @@ import { loop, setVideo } from "./upNext.js";
 
 const indexes = [
 	{
-		text: 'What is HTML',
-		video: `<iframe  src="https://www.youtube.com/embed/a2L8ra2TOMg" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'How to download and install android studio',
+		video: `<iframe  src="https://www.youtube.com/embed/roDz8mMvbIg" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Learn HTML',
-		video: `<iframe  src="https://www.youtube.com/embed/88PXJAA6szs" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'How an android application works',
+		video: `<iframe  src="https://www.youtube.com/embed/DnpZ8FYfals" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'What is CSS',
-		video: `<iframe  src="https://www.youtube.com/embed/6vbgZnQrpbU" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'Android activity lifecycle',
+		video: `<iframe  src="https://www.youtube.com/embed/S8voQap6suk" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Learn CSS',
-		video: `<iframe  src="https://www.youtube.com/embed/DFs-du7Uc2w" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'Event Handling',
+		video: `<iframe  src="https://www.youtube.com/embed/zVct6vhyiD8" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'What is Js',
+		text: 'Multiple event listeners',
 		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Learn Js',
+		text: 'What are gestures?',
 		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Beginner Js',
+		text: 'Creating design',
 		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Part 2',
+		text: 'Creating workout advisor part 1',
 		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Intermediate JS',
+		text: 'Creating workout advisor part 2',
 		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Advanced Js',
+		text: 'What is listview?',
 		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Simple website',
+		text: 'Creating simple android app part 1',
 		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
