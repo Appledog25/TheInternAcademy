dff7ce0e5d6547187aa44ac83c1249a3f6e2d5f7
Appledog25 <adititayde2509@gmail.com>
2021-10-04 16:44:07 +0530

    update

--- Task5/js/android.js
+++ Task5/js/android.js
@@ -1,53 +1,57 @@
 import { loop, setVideo } from "./upNext.js";
 
+
+
+// change this index text and video src links : )
+
 const indexes = [
 	{
-		text: 'What is DS',
-		video: `<iframe  src="https://www.youtube.com/embed/X3paOmcrTjQ" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'What is HTML',
+		video: `<iframe  src="https://www.youtube.com/embed/a2L8ra2TOMg" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Learn Python',
-		video: `<iframe  src="https://www.youtube.com/embed/rfscVS0vtbw" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'Learn HTML',
+		video: `<iframe  src="https://www.youtube.com/embed/88PXJAA6szs" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'What is Pandas',
-		video: `<iframe  src="https://www.youtube.com/embed/fAxjxoNqU9o" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'What is CSS',
+		video: `<iframe  src="https://www.youtube.com/embed/6vbgZnQrpbU" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Learn Pandas',
-		video: `<iframe  src="https://www.youtube.com/embed/dcqPhpY7tWk" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'Learn CSS',
+		video: `<iframe  src="https://www.youtube.com/embed/DFs-du7Uc2w" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'What is Numpy',
-		video: `<iframe  src="https://www.youtube.com/embed/QUT1VHiLmmI" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'What is Js',
+		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Learn Numpy',
-		video: `<iframe  src="https://www.youtube.com/embed/xECXZ3tyONo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'Learn Js',
+		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Beginner Python',
-		video: `<iframe  src="https://www.youtube.com/embed/hEgO047GxaQ" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'Beginner Js',
+		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
 		text: 'Part 2',
-		video: `<iframe  src="https://www.youtube.com/embed/WvhQhj4n6b8" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Intermediate Python',
-		video: `<iframe  src="https://www.youtube.com/embed/HGOBQPFzWKo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'Intermediate JS',
+		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
-		text: 'Advanced Python',
-		video: `<iframe  src="https://www.youtube.com/embed/tdn9_MZ0lN4" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		text: 'Advanced Js',
+		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
 		text: 'Simple website',
-		video: `<iframe  src="https://www.youtube.com/embed/BDi3SD7E6no" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 	{
 		text: 'End',
-		video: `<iframe  src="https://www.youtube.com/embed/m2__px9YJlg" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
+		video: `<iframe  src="https://www.youtube.com/embed/J4UKL355sUo" title="YouTube" frameborder="0" allowfullscreen></iframe>`,
 	},
 ];
 
